5383338a4d02bc96690823fac6ff6b59a798e6d6
Seneca Sisneros <seneca.sisneros@yahoo.com>
2017-09-13 22:38:33 -0700

    updated html structure

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const app = express();
+const express = require( 'express' );
+const app = express( );
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static( __dirname + '/' ));
 
-app.listen(process.env.PORT || 8080);
+app.listen( process.env.PORT || 8080 );
