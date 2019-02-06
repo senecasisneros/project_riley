88e0d66d467889c4b9d59b798a6cc3da99769533
Seneca Sisneros <seneca.sisneros@yahoo.com>
2019-02-05 22:11:26 -0800

    Simplified main.js and verified website is working correctly.

--- js/index.js
+++ js/index.js
@@ -1,22 +1,23 @@
-$('.slider1').bxSlider({
-  mode: 'fade',
+$(".slider1").bxSlider({
+  mode: "fade",
   captions: false,
-  auto:true,
-  pager:false,
-
+  auto: true,
+  pager: false
 });
-$('.slider2').bxSlider({
-  pager:false,
+
+$(".slider2").bxSlider({
+  pager: false,
   captions: true,
   maxSlides: 3,
   minSlides: 1,
   slideWidth: 230,
   slideMargin: 10
 });
-$('.slider3').bxSlider({
-  mode: 'fade',
+
+$(".slider3").bxSlider({
+  mode: "fade",
   captions: false,
-  auto:true,
-  pager:false,
-  controls:false,
+  auto: true,
+  pager: false,
+  controls: false
 });
